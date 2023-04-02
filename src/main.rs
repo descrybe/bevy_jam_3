b074970e1fe4621ae8c61e0d7fc64fcaf83be49f
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-04-02 20:57:05 +0400

    spawned player and camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,46 @@
-use::bevy::prelude::*;
+use::bevy::{
+    prelude::*,
+    window::PrimaryWindow,
+};
+
+pub const PLAYER_SIZE: f32 = 32.0;
 
 fn main() {
-    
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_service: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            texture: asset_service.load("sprites/ball_blue_small.png"),
+            ..default()
+        },
+        Player {},
+    ));
+}
+
+pub fn spawn_camera(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
 }
