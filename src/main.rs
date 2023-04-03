3ba027415178e24defbccf502b0ba828e15671d9
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-04-03 20:05:09 +0400

    enemy spawn

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,28 @@ use::bevy::{
     prelude::*,
     window::PrimaryWindow,
 };
+use rand::prelude::*;
 
 pub const PLAYER_SIZE: f32 = 32.0;
 pub const PLAYER_SPEED: f32 = 600.0;
+pub const ENEMY_SIZE: f32 = 32.0;
+pub const ENEMY_COUNT: u32 = 100;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_system(player_movement)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_enemy)
         .add_startup_system(spawn_camera)
         .run();
 }
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -45,6 +52,28 @@ pub fn spawn_camera(
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+pub fn spawn_enemy(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>
+) {
+    let window = window_query.get_single().unwrap();
+    let mut rng = rand::thread_rng();
+
+    for _ in 0..ENEMY_COUNT {
+        let x: f32 = rng.gen::<f32>() * window.width();
+        let y: f32 = rng.gen::<f32>() * window.height();
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(x, y, 0.0),
+                texture: asset_server.load("sprites/ball_red_small.png"),
+                ..default()
+            },
+            Enemy {}
+        ));
+    }
 }
 
 pub fn player_movement(
