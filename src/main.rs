a82fa616f2d914082e790f216d76279012afaa1e
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-04-03 22:56:07 +0400

    added enemies movement and new player sprite

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use::bevy::{
 };
 use rand::prelude::*;
 
-pub const PLAYER_SIZE: f32 = 32.0;
-pub const PLAYER_SPEED: f32 = 600.0;
-pub const ENEMY_SIZE: f32 = 32.0;
-pub const ENEMY_COUNT: u32 = 100;
+pub const PLAYER_SIZE: f32 = 16.0;
+pub const PLAYER_SPEED: f32 = 700.0;
+pub const ENEMY_SIZE: f32 = 16.0;
+pub const ENEMY_COUNT: u32 = 20;
+pub const ENEMY_SPEED: f32 = 400.0;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_system(player_movement)
+        .add_system(enemy_movement)
         .add_startup_system(spawn_player)
-        .add_startup_system(spawn_enemy)
+        .add_startup_system(spawn_enemies)
         .add_startup_system(spawn_camera)
         .run();
 }
@@ -23,7 +25,9 @@ fn main() {
 pub struct Player {}
 
 #[derive(Component)]
-pub struct Enemy {}
+pub struct Enemy {
+    pub direction: Vec2,
+}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -35,17 +39,14 @@ pub fn spawn_player(
     commands.spawn((
         SpriteBundle {
             transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-            texture: asset_service.load("sprites/ball_blue_small.png"),
+            texture: asset_service.load("sprites/player_sprite.png"),
             ..default()
         },
         Player {},
     ));
 }
 
-pub fn spawn_camera(
-    mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-) {
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
 
     commands.spawn(Camera2dBundle {
@@ -54,24 +55,26 @@ pub fn spawn_camera(
     });
 }
 
-pub fn spawn_enemy(
+pub fn spawn_enemies(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
-    let mut rng = rand::thread_rng();
 
     for _ in 0..ENEMY_COUNT {
-        let x: f32 = rng.gen::<f32>() * window.width();
-        let y: f32 = rng.gen::<f32>() * window.height();
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
         commands.spawn((
             SpriteBundle {
-                transform: Transform::from_xyz(x, y, 0.0),
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
                 texture: asset_server.load("sprites/ball_red_small.png"),
                 ..default()
             },
-            Enemy {}
+            Enemy {
+                direction: Vec2::new(random::<f32>(), random::<f32>()).normalize(),
+            },
         ));
     }
 }
@@ -111,5 +114,12 @@ pub fn player_movement(
         }
 
         transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn enemy_movement(mut enemy_query: Query<(&mut Transform, &Enemy)>, time: Res<Time>) {
+    for (mut transform, enemy) in enemy_query.iter_mut() {
+        let direction = Vec3::new(enemy.direction.x, enemy.direction.y, 0.0);
+        transform.translation += direction * ENEMY_SPEED * time.delta_seconds();
     }
 }
