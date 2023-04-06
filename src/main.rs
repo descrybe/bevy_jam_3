4f6f70596de550829d6f5599acd06289a9029d3e
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-04-06 22:07:23 +0400

    removed empty files, added player sprites

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
-use ::bevy::prelude::*;
-use events::game_over::GameOver;
-use resources::score::Score;
-use systems::{
-    camera::{camera_follow, spawn_camera},
-    enemy::{change_enemy_direction, enemy_hit_player, enemy_movement, spawn_enemies},
-    game::game_over_hander,
-    player::{change_player_direction, player_movement, spawn_player},
-};
-
-mod components;
-mod entities;
-mod events;
-mod resources;
-mod services;
+pub mod components;
+pub mod events;
+pub mod game;
+mod main_menu;
 mod systems;
+
+use game::GamePlugin;
+use main_menu::MainMenuPlugin;
+
+use events::*;
+use systems::*;
+
+use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(spawn_player)
-        .add_startup_system(spawn_enemies)
+        .add_state::<AppState>()
+        .add_plugin(MainMenuPlugin)
+        .add_plugin(GamePlugin)
         .add_startup_system(spawn_camera)
-        .add_system(player_movement)
-        .add_system(enemy_movement)
-        .add_system(enemy_hit_player)
-        .add_system(camera_follow)
-        .add_system(change_player_direction)
-        .add_system(change_enemy_direction)
+        // .add_system(camera_follow)
         .add_event::<GameOver>()
-        .init_resource::<Score>()
         .add_system(game_over_hander)
         .run();
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
 }
