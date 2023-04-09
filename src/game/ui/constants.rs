26b7379ae7425796f8267b9434b0586fafc3421e
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-04-09 20:34:01 +0400

    experience bar proto

--- src/game/ui/constants.rs
+++ src/game/ui/constants.rs
@@ -12,16 +12,20 @@ pub const PLAYER_HEALTH_BAR_WIDTH:Val = Val::Px(100.0);
 pub const PLAYER_HEALTH_BAR_HEIGHT:Val = Val::Px(10.0);
 
 pub const EXPERIENCE_BAR_WRAPPER_STYLES: Style = Style {
+    // size: Size {
+    //     width: Val::Percent(100.0),
+    //     height: EXPERIENCE_BAR_HEIGHT,
+    // },
     size: Size {
-        width: Val::Percent(100.0),
-        height: EXPERIENCE_BAR_HEIGHT,
+        width: Val::Px(100.0),
+        height: Val::Px(100.0),
     },
     position_type: PositionType::Absolute,
     position: UiRect {
-        top: Val::Auto,
+        top: Val::Px(20.0),
         left: Val::Auto,
         right: Val::Auto,
-        bottom: Val::Px(20.0),
+        bottom: Val::Auto,
     },
     align_self: AlignSelf::FlexEnd,
     justify_content: JustifyContent::Center,
