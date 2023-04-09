67340e6aef96b0994ec70aaee65e695d765cca28
UhBroSeemOff <fantomklin@outlook.com>
2023-04-09 16:13:40 +0600

    Extended HealthComponent

--- src/game/health/components.rs
+++ src/game/health/components.rs
@@ -3,6 +3,7 @@ use bevy::prelude::Component;
 #[derive(Component)]
 pub struct HealthComponent {
     amount: i32,
+    max_health: i32,
 }
 
 impl HealthComponent {
@@ -14,7 +15,29 @@ impl HealthComponent {
         self.amount -= amount;
     }
 
+    pub fn heal(&mut self, amount: i32) {
+        let result = self.amount + amount;
+
+        if result > self.max_health {
+            self.amount = self.max_health;
+        } else {
+            self.amount = result;
+        }
+    }
+
+    pub fn recover(&mut self) {
+        self.amount = self.max_health;
+    }
+
+    pub fn increase_max_health(&mut self, amount: i32) {
+        self.max_health += amount;
+        self.heal(amount);
+    }
+
     pub fn new(amount: i32) -> Self {
-        HealthComponent { amount }
+        HealthComponent {
+            amount,
+            max_health: amount,
+        }
     }
 }
