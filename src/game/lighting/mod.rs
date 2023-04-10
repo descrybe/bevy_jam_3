34df22a22260c440506ba8e86555884b20268250
Anton Yushmanov <yushmanov.anton@xunison.com>
2023-04-10 20:01:21 +0400

    Add lighting animation

--- src/game/lighting/mod.rs
+++ src/game/lighting/mod.rs
@@ -1,7 +1,7 @@
 pub mod components;
 mod systems;
 
-use self::systems::spawn_lightning_bolts;
+use self::systems::{spawn_lightning_bolts, animate_lighting};
 
 use super::GameSimulationState;
 use crate::AppState;
@@ -12,8 +12,8 @@ pub struct LightingPlugin;
 
 impl Plugin for LightingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(
-            (spawn_lightning_bolts)
+        app.add_systems(
+            ((spawn_lightning_bolts, animate_lighting))
                 .in_set(OnUpdate(AppState::Game))
                 .in_set(OnUpdate(GameSimulationState::Running)),
         );
